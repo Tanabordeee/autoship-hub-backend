75861f65f1716f5d0eb18f93729a978d7a8b23c6
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2026-01-27 00:05:56 +0700

    refactor : ocr image

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,15 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_HOURS: int
     DATABASE_URL: str
+    OCR_MODEL: str = "scb10x/typhoon-ocr1.5-3b:latest"
+    POPPLER_PATH: str = r"E:\poppler\poppler-25.12.0\Library\bin"
 
-    model_config = {
-        "env_file": ".env",
-        "env_file_encoding": "utf-8",
-        "extra": "ignore"
-    }
+    model_config = {"env_file": ".env", "env_file_encoding": "utf-8", "extra": "ignore"}
+
 
 settings = Settings()
