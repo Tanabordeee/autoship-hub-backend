d5bd6a4e233a272b2a7927822c86193a1983e58e
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2026-01-27 18:26:50 +0700

    refactor : remove import unuse

--- app/schemas/transaction.py
+++ app/schemas/transaction.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
-from typing import List
+
 
 class TransactionCreate(BaseModel):
     status: str
     current_process: str
+
 
 class TransactionUpdate(BaseModel):
     status: str
