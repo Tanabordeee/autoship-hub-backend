3a8d4ad46a0cb836a1d1e5660e633bcc667b7335
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2026-01-27 22:05:47 +0700

    feat : create Booking

--- app/api/v1/endpoints/booking.py
+++ app/api/v1/endpoints/booking.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, UploadFile, File
+from fastapi import APIRouter, Depends, UploadFile, File, Form, Body
 from app.api.deps import get_db
 
 # from app.services.booking_service import create_booking
@@ -8,23 +8,31 @@ from app.services.booking.service import extract_booking
 from sqlalchemy.orm import Session
 from app.api.deps import get_current_user
 from app.models.user import User
+from app.schemas.booking import CreateBooking
+from app.services.booking.service import create_booking
 
 router = APIRouter()
 
 
-# @router.post("/bookings", response_model=str)
-# def create_booking_endpoint(
-#     payload: CreateBooking,
-#     db: Session = Depends(get_db),
-#     current_user: User = Depends(get_current_user),
-# ):
-#     return create_booking(db, payload, current_user.id)
+@router.post("/bookings")
+def create_booking_endpoint(
+    payload: CreateBooking = Body(..., embed=True),
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    transaction_id = payload.transaction_id
+    try:
+        transaction_id = int(transaction_id)
+    except (ValueError, TypeError):
+        transaction_id = 0
+    return create_booking(db, payload, current_user.id, transaction_id)
 
 
 @router.post("/extract-booking")
 def extract_booking_endpoint(
+    transaction_id: str = Form(...),
     file: UploadFile = File(...),
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-    return extract_booking(file)
+    return extract_booking(db, file, transaction_id)
