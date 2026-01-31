c03e0a88ecb7cea303a04c8a847e90f2447f4fbf
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2026-01-31 18:15:11 +0700

    feat : create Schema

--- app/schemas/bl.py
+++ app/schemas/bl.py
@@ -7,3 +7,27 @@ class BLCheck(BaseModel):
     vehicle_register_id: int
     pi_id: int
     si_id: int
+
+
+class BLCreate(BaseModel):
+    user_id: int
+    version_bl: str
+    bl_number: str
+    jo_number: str
+    shipper: str
+    consignee: str
+    notify_party: str
+    place_of_receipt: str
+    port_of_loading: str
+    port_of_discharge: str
+    ocean_vessel: str
+    place_of_delivery: str
+    freight_payable_at: str
+    number_of_original_bl: str
+    gross_weight: str
+    measurement: str
+    cy_cf: str
+    description_of_goods: str
+    container: str
+    seal_no: str
+    size: str
